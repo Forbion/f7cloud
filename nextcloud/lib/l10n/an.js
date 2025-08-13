199c407fba7794ca6d24bee838d3c72d21a8e6b8
root <root@prod-gen.f7cloud.ru>
2025-08-13 10:27:10 +0000

    all nextcloud dir

---
+++ nextcloud/lib/l10n/an.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "lib",
+    {
+    "See %s" : "Veyer %s",
+    "Unknown filetype" : "Tipo de fichero esconoxiu",
+    "Invalid image" : "Imachen no valida",
+    "Files" : "Archivos",
+    "today" : "Hue",
+    "yesterday" : "Ayer",
+    "last month" : "Zaguero mes",
+    "last year" : "Zaguero año",
+    "Apps" : "Aplicazions",
+    "Settings" : "Configurazión",
+    "Email" : "Correu electronico",
+    "Users" : "Usuarios"
+},
+"nplurals=2; plural=(n != 1);");
